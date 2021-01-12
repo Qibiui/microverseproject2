cb1e30654f92c2f23d4afba3d7e3c1847c10838e
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-01-12 14:07:46 +0000

    Create LICENSE.TXT

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -1,4 +1,8 @@
 var init_friend_lookup;
 
 init_friend_lookup = function(){
-  $('#friend-lookup-form')
+  $('#friend-lookup-form').on('ajax:before', function(event, data, status){
+    show_spinner();
+  });
+
+  $('#friend-lookup-form').
