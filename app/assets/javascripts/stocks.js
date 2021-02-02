a126eb8c841a0e84a2b5b7b1cb63251766353c56
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-02-02 22:02:42 +0000

    update solutions

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -2,4 +2,8 @@ var init_stock_lookup;
 
 init_stock_lookup = function(){
   $('#stock-lookup-form').on('ajax:before', function(event, data, status){
-    show_
+    show_spinner();
+  });
+
+  $('#stock-lookup-form').on('ajax:after', function(event, data, status){
+    hide_spinner();
