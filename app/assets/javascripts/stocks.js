0ad8ec3e32a052b785590d311be3ff28aff1bdc9
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-02-19 18:33:26 +0000

    net: neighbour: fix neigh_dump_info()

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -7,3 +7,11 @@ init_stock_lookup = function(){
 
   $('#stock-lookup-form').on('ajax:after', function(event, data, status){
     hide_spinner();
+  });
+
+  $('#stock-lookup-form').on('ajax:success', function(event, data, status){
+    $('#stock-lookup').replaceWith(data);
+    init_stock_lookup();
+  });
+
+  $('#stock-lookup-form
