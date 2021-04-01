1263118cfb7da021209469c212dc6950df46b643
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-01 06:26:26 +0000

    new protocol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,3 +2,5 @@ class StocksController < ApplicationController
 
   def search
     if params[:stock]
+      @stock = Stock.find_by_ticker(params[:stock])
+      @stock ||= Stock.new_from_lookup(params[:stock
