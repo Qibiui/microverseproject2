c49c0239f6172d2a3db63f63fe5e7bfb149947c9
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-01 13:59:50 +0000

    fix comment typo

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,4 +3,11 @@ class StocksController < ApplicationController
   def search
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock])
-      @stock ||= Stock.new_from_lookup(params[:stock
+      @stock ||= Stock.new_from_lookup(params[:stock])
+    end
+
+    if @stock
+      #render json: @stock
+      render partial: 'lookup'
+    else
+      render status: :no
