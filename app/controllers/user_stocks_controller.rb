5382a299fc43b2162133440bf1d069e19ea8b977
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-03 08:40:39 +0000

    fix composer json

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -2,4 +2,10 @@ class UserStocksController < ApplicationController
   before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
 
   # GET /user_stocks
-  # GET /user_stock
+  # GET /user_stocks.json
+  def index
+    @user_stocks = UserStock.all
+  end
+
+  # GET /user_stocks/1
+  #
