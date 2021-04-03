6f500ace1307b2829af6fdeeb0aef41294b60620
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-03 07:56:01 +0000

    Test..

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,1 +1,5 @@
-class UserStocksController < ApplicationControlle
+class UserStocksController < ApplicationController
+  before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
+
+  # GET /user_stocks
+  # GET /user_stock
