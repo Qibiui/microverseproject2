550f21ef8f18a8471c73831123660057e53fb57c
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-24 19:06:28 +0000

    update req

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -73,4 +73,6 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to my_por
+      format.html { redirect_to my_portfolio_path, notice: 'Stock was successfully removed from portfolio.' }
+      format.json { head :no_content }
+   
