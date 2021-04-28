d8ad2860717027f9fcbbe6922229afcfb22d63f5
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-28 20:04:45 +0000

    Multiple Fixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,7 @@ class UsersController < ApplicationController
     @users = User.search(params[:search_param])
 
     if @users
-      @users = current_user.exc
+      @users = current_user.except_current_user(@users)
+      render partial: "friends/lookup"
+    else
+      render status: :not_found, nothing: t
