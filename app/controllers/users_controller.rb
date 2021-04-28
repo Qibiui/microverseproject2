934f397a7dc93fdab4e028998c659b59e84f6e92
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-28 15:42:47 +0000

    Run scan-local.sh on nuxeo:...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def my_portfolio
     @user_stocks = current_user.stocks
-    @user 
+    @user = current_user
+  end
+
+  def my_friends
+    @friendships = current_user.friends
+  end
+
+  def se
