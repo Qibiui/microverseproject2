7f04ebda27f32faa51cfb768739251d240f4d73d
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-04-29 23:09:22 +0000

    update issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,6 @@ class UsersController < ApplicationController
     current_user.friendships.build(friend_id: @friend.id)
 
     if current_user.save
-      redirect_to my_friends_path, notice: "Fri
+      redirect_to my_friends_path, notice: "Friend was successfully added"
+    else
+      redirect_to my_friends_path, flash[:error] = "There
