5b5229231f9dc5b781e3e9f55cd6213de16650e1
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-05-09 21:02:42 +0000

    Update labels

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-  has_many :fri
+  has_many :friendships
+  has_many :friends, through: :friendships
+  
+  
+  def full_name
+    return "#{first_name} #{last_na
