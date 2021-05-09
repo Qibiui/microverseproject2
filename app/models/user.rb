67a32c8b05d9f04629911e75f1f63c96c7da45b3
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-05-09 09:27:00 +0000

    One more tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
- 
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  has_many :fri
