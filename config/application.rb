d6200bb82e7b83ecefea8724587c66ea3d423f95
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-11 13:54:47 +0000

    defconfig: update

--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, includ
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups
