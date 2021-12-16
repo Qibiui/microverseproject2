7c8a6bce24a3d836164c56f448f4d0ba95491355
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-16 16:23:34 +0000

    Adjust margins

--- config/application.rb
+++ config/application.rb
@@ -14,3 +14,6 @@ module FinanceTracker
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all tr
