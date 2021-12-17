b91e0384e747ca7eacb002e53da71ad2c2abf723
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-17 05:43:21 +0000

    build: support Node.js 8.x

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,5 @@ module FinanceTracker
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all tr
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += D
