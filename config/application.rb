ddf4859f21d8f17994d8b8c9fd23f24ddf957831
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-17 21:03:19 +0000

    Fixing build break

--- config/application.rb
+++ config/application.rb
@@ -18,4 +18,6 @@ module FinanceTracker
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
