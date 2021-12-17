245091a04e76ae4848ebe36dfe1a05cb11a6edb7
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-17 11:33:18 +0000

    net: dp83640: fix broken calibration routine.

--- config/application.rb
+++ config/application.rb
@@ -17,4 +17,5 @@ module FinanceTracker
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += D
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18
