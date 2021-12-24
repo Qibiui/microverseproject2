15ab9421226cf10e7399e9c5922f15c3000a2bc7
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-24 11:33:52 +0000

    ALSA: hda - Add new codec ALC283 ALC290 support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,2 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will 
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment y
