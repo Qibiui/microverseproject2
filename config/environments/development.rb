d8692b905e48c21dd349dabdf10ba2f72e14fb89
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-26 14:10:04 +0000

    Example tweak

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,4 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  conf
+  config.eager_load = false
+
+  # Show full error reports and disable caching.
+  config.consider_all_re
