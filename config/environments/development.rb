73df5d047697aa3691bd4e8508909a87731c9054
Qibiui <92776161+Qibiui@users.noreply.github.com>
2021-12-29 16:00:15 +0000

    STYLE: Nightly Version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,7 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_loa
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significan
