32c2cda95e2cd3fe7bb1f2f1bab455762477536f
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-01-08 07:27:28 +0000

    Last commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precede
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between re
