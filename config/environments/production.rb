620d2855ad44b95ed3a0e647572f82931176e285
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-01-09 07:57:02 +0000

    Version 0.9.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,4 +7,8 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for pe
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
+  co
