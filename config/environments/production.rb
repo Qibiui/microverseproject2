77d4b21477b9bdea06f1b5b452a52f19f65e5c17
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-01-14 07:01:52 +0000

    updating gitignore file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,4 +26,6 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.cs
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled as
