65dd982009c5c80be8c5a43efe394fd3d0dc8a9c
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-01-20 08:38:00 +0000

    put the PDB texts back into the spec files. summaries first. From now on the SVN is the master source for the texts as far as factory (:head) is concerned. Old products will continue to overwrite the texts from PDB. %descriptions follow next

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,4 +45,8 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
-  # when problems aris
+  # when problems arise.
+  config.log_level = :debug
+
+  # Prepend all log lines with the following tags.
+  #
