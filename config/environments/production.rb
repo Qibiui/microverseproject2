b96045dbad61f63296a8c682a80128c7eb996e1a
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-01-24 09:10:00 +0000

    get rid of kern_path_parent()

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,5 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not 
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immedia
