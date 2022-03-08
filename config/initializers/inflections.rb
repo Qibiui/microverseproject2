37281c85b167a4e8d296824caa034c202921ad5b
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-03-08 13:35:02 +0000

    BUILD-INERA: Set explicit released versions

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,4 +11,6 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflection
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
