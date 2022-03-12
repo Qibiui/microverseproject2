2fa8ae727a68f7df13f684e9042ae1914d0fff66
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-03-12 13:13:52 +0000

    add print

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# This file contains settings for ActionController::ParamsW
+# This file contains settings for ActionController::ParamsWrapper which
+# is enabled by default.
+
+# Enable parameter wrapping for JSON. You can disable thi
