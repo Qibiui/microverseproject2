ac129bb4e86007235cfc2949ce8206923596b17a
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-06-08 19:55:44 +0000

    Update Travis config.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index
+  root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
+  get 'm
