f3d1edaddff64bf935d056013dbf4afd50e63b03
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-06-09 12:23:02 +0000

    Fixed Readme

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
-  get 'm
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#sea
