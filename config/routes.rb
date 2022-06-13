2289f48f2cc43f7c7fdb0d9ebcd7bf7fe5397b6f
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-06-13 12:58:34 +0000

    Added code of conduct

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(i
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example
