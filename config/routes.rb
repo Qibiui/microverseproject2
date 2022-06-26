72b069462745bcefde2d5b41c289e38a26c7fc9b
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-06-26 15:27:02 +0000

    block: do not pass disk names as format strings

--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,6 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespac
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app
