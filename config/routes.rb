bef9ac6a06782faefa78d3d34892651a2c96e2a5
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-06-27 11:32:23 +0000

    mediainfo changes

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,7 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
