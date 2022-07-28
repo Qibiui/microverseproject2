ff31e78b3427d80e56aea69e276265296916a371
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-07-28 09:26:55 +0000

    Small

--- db/migrate/20160324215941_devise_create_users.rb
+++ db/migrate/20160324215941_devise_create_users.rb
@@ -16,4 +16,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.string
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmatio
