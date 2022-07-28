d42fe056fc778256d97fca4a8b76e50e6381a732
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-07-28 07:42:33 +0000

    update gem description

--- db/migrate/20160324215941_devise_create_users.rb
+++ db/migrate/20160324215941_devise_create_users.rb
@@ -14,4 +14,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
-  
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string
