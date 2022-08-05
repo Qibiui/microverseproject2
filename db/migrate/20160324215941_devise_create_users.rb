339fd295800820a3ff9a3bd44da92b5849f62aec
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-05 16:20:30 +0000

    Fix minor typo.

--- db/migrate/20160324215941_devise_create_users.rb
+++ db/migrate/20160324215941_devise_create_users.rb
@@ -34,4 +34,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :users, :email,  
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index
