8eae02a267ee1104b99ca928bd17c99ae857eab8
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-06 05:46:53 +0000

    masthead

--- db/migrate/20160324215941_devise_create_users.rb
+++ db/migrate/20160324215941_devise_create_users.rb
@@ -36,4 +36,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         un
