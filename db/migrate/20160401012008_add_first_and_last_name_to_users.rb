bc68171bc522e149ad7a2f680b9a976b7cd44d43
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-08 17:06:56 +0000

    updated en.yml

--- db/migrate/20160401012008_add_first_and_last_name_to_users.rb
+++ db/migrate/20160401012008_add_first_and_last_name_to_users.rb
@@ -1,1 +1,4 @@
-class AddFirstAndLastNameToUsers < ActiveRecord::Migrati
+class AddFirstAndLastNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :u
