3dab3e109fe2e4b9004b92a2e9e608b2fd5ee59a
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-21 21:41:03 +0000

    remove module

--- db/schema.rb
+++ db/schema.rb
@@ -3,4 +3,5 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the auth
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
