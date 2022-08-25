28dabe76e801d7ea10e7c39b3ff7957c078d22fc
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-25 18:27:59 +0000

    Bump snapshot (#5441)

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(versi
+ActiveRecord::Schema.define(version: 20160405203824) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t
