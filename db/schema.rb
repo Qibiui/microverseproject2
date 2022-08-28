1a72db49ce1d5d87b9163dbb3bc7c33be40cb03b
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-08-28 07:18:07 +0000

    Updated comment.

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,6 @@ ActiveRecord::Schema.define(version: 20160405203824) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
-    t
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "upd
