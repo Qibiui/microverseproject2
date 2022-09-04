38ca1a0b126a60f3a29415ae6aea41e982135bbe
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-04 15:56:12 +0000

    ChangeLog:

--- db/schema.rb
+++ db/schema.rb
@@ -36,4 +36,5 @@ ActiveRecord::Schema.define(version: 20160405203824) do
   end
 
   create_table "users", force: :cascade do |t|
-  
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_pass
