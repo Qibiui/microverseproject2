601e4a0b2b0ac453b6ad754c6cec54e9b6d8201a
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-06 19:07:30 +0000

    0.8.7

--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,6 @@ ActiveRecord::Schema.define(version: 20160405203824) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_pass
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_p
