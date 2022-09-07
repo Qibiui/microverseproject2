83a4ad08822c7d41444d78768dff0a2726418856
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-07 17:43:56 +0000

    change condition

--- db/schema.rb
+++ db/schema.rb
@@ -39,4 +39,6 @@ ActiveRecord::Schema.define(version: 20160405203824) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_p
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",      
