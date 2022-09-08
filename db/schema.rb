8cd4cb45b8ba4738c0a6790ebd70b91a452fac7e
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-08 13:19:54 +0000

    pie chart

--- db/schema.rb
+++ db/schema.rb
@@ -44,4 +44,6 @@ ActiveRecord::Schema.define(version: 20160405203824) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",   
