ac5c2ebede1a607e8ea7d88f306839b4daa277d0
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-08 12:57:20 +0000

    New translations 03_p01_ch01_01.md (Tagalog)

--- db/schema.rb
+++ db/schema.rb
@@ -41,4 +41,7 @@ ActiveRecord::Schema.define(version: 20160405203824) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",      
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.
