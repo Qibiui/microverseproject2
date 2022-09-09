7f71ee290b6df80d55a408b5c3da84503fa7a7c8
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-09 08:03:31 +0000

    Fix paths

--- db/schema.rb
+++ db/schema.rb
@@ -46,4 +46,5 @@ ActiveRecord::Schema.define(version: 20160405203824) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",   
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                    
