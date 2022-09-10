0f949c3a524f905d26b2b466c31db283224bd8d2
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-10 08:18:54 +0000

    extensions

--- db/schema.rb
+++ db/schema.rb
@@ -52,4 +52,5 @@ ActiveRecord::Schema.define(version: 20160405203824) do
     t.string   "last_name"
   end
 
-  add_index "users", ["em
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], nam
