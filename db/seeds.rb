27c73769670754c242898e7e0896739be81a845f
Qibiui <92776161+Qibiui@users.noreply.github.com>
2022-09-13 11:48:36 +0000

    Add scrutinizer config

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhage
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
