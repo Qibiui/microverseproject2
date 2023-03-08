a21e17298443de3e3e184d72e3714546ac54d18b
Qibiui <92776161+Qibiui@users.noreply.github.com>
2023-03-08 06:00:01 +0000

    cleans up

--- test/controllers/user_stocks_controller_test.rb
+++ test/controllers/user_stocks_controller_test.rb
@@ -2,3 +2,9 @@ require 'test_helper'
 
 class UserStocksControllerTest < ActionController::TestCase
   setup do
+    @user_stock = user_stocks(:one)
+  end
+
+  test "should get index" do
+    get :index
+    
